2219f5163bc3d137b72ec3620ce2c2eda79dfb47
demaio89 <demaio89@gmail.com>
2021-10-15 08:12:12 -0500

    creating and designing app footer

--- src/app/components/car/index.tsx
+++ src/app/components/car/index.tsx
@@ -6,6 +6,9 @@ import tw from 'twin.macro';
 import { ICar } from '../../../typings/car';
 import { Button } from '../button';
 
+
+
+
 interface ICarProps extends ICar{}
 
 const CarContainer = styled.div`
