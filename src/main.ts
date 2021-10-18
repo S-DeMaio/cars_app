b07cf9d774d3445f5f03abe4771b4c42aa9ffa99
demaio89 <demaio89@gmail.com>
2021-10-18 18:14:27 -0500

    Add validation to new car input fields GraphQL

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { resolve } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 dotenv.config({ path: resolve(__dirname, '../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-app.useGlobalFilters(new BaseExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
 
   await app.listen(process.env.PORT || 9000);
 }
