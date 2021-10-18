0367b105b6b5941e4e68faabd39163785b10be70
demaio89 <demaio89@gmail.com>
2021-10-18 18:02:23 -0500

    Creating car entity, fetching cars from database & creating Add new car mutation with GQL input

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
+dotenv.config({ path: resolve(__dirname, '../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+app.useGlobalFilters(new BaseExceptionFilter());
+
   await app.listen(process.env.PORT || 9000);
 }
 bootstrap();
